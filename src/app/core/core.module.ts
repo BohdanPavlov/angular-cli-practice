01bc57c3de35163fdb1522fe3e8439688721ea4f
Bohdan Pavlov <bogdanpavlov77@gmail.com>
2022-09-07 15:03:23 +0300

    created folders for components and services in core directory

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+
+import {AuthenticationService} from "./services/authentication.service";
 
 
 @NgModule({
-    declarations: [
-        HeaderComponent,
-        FooterComponent
-    ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   exports: [
     HeaderComponent,
     FooterComponent
   ],
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    AuthenticationService
+  ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
